e0cbc75de7b4b694b16802903f1e172988ad32bf
Thayssth <ts2805004@gmail.com>
2024-11-26 13:48:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual jogador mais rico do mundo?',
+    'cristiano Ronaldo'
+)
+
+criaCartao(
+    'jardinagem',
+    'Qual a flor mais cheirosa do mundo?',
+    'Lavanda'
+)
+
+criaCartao(
+    'Animais',
+    'Qual o animal mais dorminhoco do mundo?',
+    'Coala'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual pais e considerado mais rico?',
+    'Luxemburgo'
+)
